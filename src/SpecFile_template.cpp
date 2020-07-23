943aced0f8452d0131f45ee53a3d79b0e8933bf5
Bryce Eldridge <bdeldri@sandia.gov>
2020-07-23 12:27:48 -0600

    Added time formatting utility, latitude, longitude, speed variables

--- src/SpecFile_template.cpp
+++ src/SpecFile_template.cpp
@@ -1,17 +1,17 @@
 /**
  SpecUtils: a library to parse, save, and manipulate gamma spectrum data files.
  Copyright (C) 2016 William Johnson
- 
+
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
  version 2.1 of the License, or (at your option) any later version.
- 
+
  This library is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  Lesser General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public
  License along with this library; if not, write to the Free Software
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -35,6 +35,7 @@
 #include <stdexcept>
 #include <algorithm>
 #include <functional>
+#include <ctime>
 
 
 #include "SpecUtils/SpecFile.h"
@@ -54,11 +55,17 @@ namespace SpecUtils
 	void to_json(json& j, shared_ptr<const SpecUtils::Measurement> p)
 	{
 		j["detector_ecal_coeffs"] = p->calibration_coeffs();
-  
+
 		j["real_time"] = p->real_time();
 		j["live_time"] = p->live_time();
 
-		j["start_time"] = to_extended_iso_string(p->start_time());
+		j["start_time_iso"] = to_extended_iso_string(p->start_time());
+
+		if (!p->start_time().is_not_a_date_time()) 
+		{
+			std::tm time_tm = to_tm(p->start_time());
+			j["start_time_raw"] = mktime(&time_tm);
+		}
 
 		j["gamma_counts"] = (*(p->gamma_counts()));
 		j["neutron_counts"] = p->neutron_counts();
@@ -70,6 +77,10 @@ namespace SpecUtils
 
 		j["detector_name"] = p->detector_name();
 		j["detector_type"] = p->detector_type();
+
+		j["latitude"] = p->latitude();
+		j["longitude"] = p->longitude();
+		j["speed"] = p->speed();
 	}
 
 	void to_json(json& j, SpecUtils::DetectorAnalysisResult p)
@@ -95,7 +106,7 @@ namespace SpecUtils
 			j["algorithm_result_description"] = p->algorithm_result_description_;
 			j["algorithm_version_components"] = p->algorithm_component_versions_;
 		}
-		else 
+		else
 		{
 			j["results"] = std::vector<SpecUtils::DetectorAnalysisResult>();
 		}
@@ -114,7 +125,19 @@ namespace SpecUtils
 			float value = args.at(1)->get<float>();
 			snprintf(buffer, sizeof(buffer), format.c_str(), value);
 			return std::string(buffer);
-		});
+			});
+
+		env.add_callback("format_time", 2, [](Arguments& args) {
+			char buffer[256];
+			std::string format = args.at(0)->get<string>();
+
+			// Convert passed in value (reverse of mktime above in template variables, assume local time) to a time_tm for formatting
+			time_t value = args.at(1)->get<time_t>();
+			std::tm* time_tm = localtime(&value);
+			strftime(buffer, sizeof(buffer), format.c_str(), time_tm);
+
+			return std::string(buffer);
+			});
 
 		// Convert a value in seconds to the N42 format PT<minutes>M<seconds>S
 		// Second argument is for seconds precision
@@ -129,14 +152,21 @@ namespace SpecUtils
 			string totalFormat = "PT%dM%" + string(sformat) + "fS";
 			snprintf(buffer, sizeof(buffer), totalFormat.c_str(), minutes, remainingSeconds);
 			return std::string(buffer);
-		});
+			});
 
 		// Run the counted zeros compression on the given vector
 		env.add_callback("compress_countedzeros", 1, [](Arguments& args) {
 			vector<float> compressed_counts;
 			compress_to_counted_zeros(args.at(0)->get<std::vector<float>>(), compressed_counts);
 			return compressed_counts;
-		});
+			});
+
+		// Doesn't seem to be any basic math, may need to expand on this
+		env.add_callback("subtract", 2, [](Arguments& args) {
+			float value1 = args.at(0)->get<float>();
+			float value2 = args.at(1)->get<float>();
+			return value1 - value2;
+			});
 
 		// STEP 1 - read template file
 		Template temp;
@@ -180,7 +210,7 @@ namespace SpecUtils
 		}
 
 		// STEP 3 - render template using JSON data to the provided stream
-		try 
+		try
 		{
 			env.render_to(ostr, temp, data);
 		}
