sandialabs/SpecUtils

commit 920c045e3b818138f58a040f8cd8d653ef3d5f27

Added and improved some energy calibration related routines. Added polynomial_energy(...) function. Improved correctness of fullrangefraction_energy(...) Added some more unit tests for these functions. Added member functions EnergyCalibration::channel_for_energy(float) and EnergyCalibration::energy_for_channel(float) to allow converting between energy and channel number, and vice versa, consistent to do, no matter the type of energy calibration.

src/SpecFile_pcf.cpp
@@ -151,48 +151,68 @@ void SpecFile::pcf_file_channel_info( size_t &nchannel,
   for( const auto &meas : measurements_ )
   {
     const size_t nmeaschann = meas->num_gamma_channels();
-    const auto &these_energies = meas->gamma_channel_energies();
+    const auto &cal = meas->energy_calibration_;
+    if( !cal
+        || (cal->type()==EnergyCalType::InvalidEquationType)
+        || (cal->num_channels() <= 7) )
+      continue;
     
-    if( nmeaschann > 7 )
-    {
-      nchannel = std::max( nchannel, nmeaschann );
+    const size_t ncalchannel = cal->num_channels();
+    assert( ncalchannel == nmeaschann );
+    
+    const auto &these_energies = cal->channel_energies();
+    assert( these_energies );
       
-      if( (these_energies->size()+1)<nmeaschann
-         || meas->energy_calibration_model() != SpecUtils::EnergyCalType::LowerChannelEdge )
+    nchannel = std::max( nchannel, nmeaschann );
+    
+    if( (ncalchannel < nmeaschann)
+       || (meas->energy_calibration_model() != SpecUtils::EnergyCalType::LowerChannelEdge) )
+    {
+      use_lower_channel = false;
+      nchannel = 0;
+      return;
+    }
+    
+    //If we have already found a lower_e_bin, check if this current
+    //  one is either the same  one in memory, or if not, if its reasonably
+    //  close in numeric value.
+    if( use_lower_channel && lower_e_bin && (lower_e_bin!=these_energies) )
+    {
+      if( lower_e_bin->size() != these_energies->size() )
       {
         use_lower_channel = false;
+        lower_e_bin.reset();
+        nchannel = 0;
+        return;
       }
       
-      //If we have already found a lower_e_bin, check if this current
-      //  one is either the same  one in memory, or if not, if its reasonably
-      //  close in numeric value.
-      if( use_lower_channel && lower_e_bin && (lower_e_bin!=these_energies) )
+      for( size_t channel = 0; use_lower_channel && channel < lower_e_bin->size(); ++channel )
       {
-        if( lower_e_bin->size() != these_energies->size() )
+        //A better float test would be AlmostEquals() from
+        //  https://github.com/abseil/googletest/blob/master/googletest/include/gtest/internal/gtest-internal.h
+        const float lhs = (*lower_e_bin)[channel];
+        const float rhs = (*these_energies)[channel];
+        
+        if( fabs(lhs-rhs) > std::max(FLT_EPSILON,0.001f*lhs) )
         {
           use_lower_channel = false;
           lower_e_bin.reset();
+          nchannel = 0;
+          return;
         }
-        
-        for( size_t channel = 0; use_lower_channel && channel < lower_e_bin->size(); ++channel )
-        {
-          //A better float test would be AlmostEquals() from
-          //  https://github.com/abseil/googletest/blob/master/googletest/include/gtest/internal/gtest-internal.h
-          const float lhs = (*lower_e_bin)[channel];
-          const float rhs = (*these_energies)[channel];
-          
-          if( fabs(lhs-rhs) > std::max(FLT_EPSILON,0.001f*lhs) )
-          {
-            use_lower_channel = false;
-            lower_e_bin.reset();
-          }
-        }//for( size_t channel = 0; use_lower_channel && channel < lower_e_bin->size(); ++channel )
-      }else if( use_lower_channel && (lower_e_bin!=these_energies) )
-      {
-        lower_e_bin = these_energies;
-      }//if( we already have found lower_e_bin ) / else if( we might us the lower channel energies )
-    }//if( nmeaschann > 7 )
+      }//for( size_t channel = 0; use_lower_channel && channel < lower_e_bin->size(); ++channel )
+    }else if( use_lower_channel && (lower_e_bin!=these_energies) )
+    {
+      lower_e_bin = these_energies;
+    }//if( we already have found lower_e_bin ) / else if( we might us the lower channel energies )
   }//for( const auto &meas : measurements_ )
+  
+  if( !use_lower_channel || nchannel <= 7 || !lower_e_bin || lower_e_bin->size() <= 7 )
+  {
+    nchannel = 0;
+    return;
+  }
+  
   
   if( use_lower_channel && nchannel > 7 && lower_e_bin && lower_e_bin->size() > 7 )
   {

SpecUtils/EnergyCalibration.h
@@ -202,6 +202,29 @@ namespace SpecUtils
     size_t memmorysize() const;
     
     
+    /** Returns the fractional channel that cooresponds to energy.
+     
+     Throws exception if #EnergyCalType::InvalidEquationType, or if #EnergyCalType::LowerChannelEdge
+     and energy is outside range, or if outside range is valid for Polynomial and FullRangeFraction.
+     
+     For Polynomial or FullRangeFraction returned value may be below 0, or above number of channels.
+     */
+    float channel_for_energy( const float energy ) const;
+    
+    /** Returns the energy cooresponding to the fractional channel passed in.
+     
+     Note: for polynomial and full range fraction will return an answer, even if channel is less
+           than zero or greater than the number of channels.  No check is made that the channel
+           is in valid range (e.g., that quadratic order term isnt overpowering linear term.).
+     
+     Throws exception if #EnergyCalType::InvalidEquationType, or if #EnergyCalType::LowerChannelEdge
+     and channel is less than zero or greater than number of channels+1, or if outside range is
+     valid for Polynomial and FullRangeFraction.
+     */
+    float energy_for_channel( const float channel ) const;
+    
+    
+    
     /// \TODO: add channel_energy( size_t channel ), find_channel( float energy ) and similar
     
 #if( PERFORM_DEVELOPER_CHECKS )
@@ -307,7 +330,10 @@ namespace SpecUtils
    @param deviation_pairs The sorted deviation pairs defined for the calibration.
    @returns The energy of the specified channel.
    
-   Doesnt perform a check that the coefficients or deviation pairs are actually valid.
+   Note: doesnt perform a check that the coefficients or deviation pairs are actually valid.
+   Note: doesnt check that channel_number passed in is valid for given coefficients (e.g.,
+         quadratic term is overpowering linear term, etc).
+   
    Throws exception if deviation pairs are not sorted.
    */
   float polynomial_energy( const float channel_number,
@@ -361,7 +387,7 @@ namespace SpecUtils
    float channel = find_fullrangefraction_channel( frf_energy, frf_coeffs, nchannel, {} );
    \endcode
    
-   ToDo: Currently will return answer accurate within about 0.01 keV, but in the
+   \TODO: Currently will return answer accurate within about 0.01 keV, but in the
          future this should be able to be made exactly accuate (within numerical
          limits anyway).
    */
@@ -467,15 +493,15 @@ namespace SpecUtils
    specified energy.
    
    @param energy The energy to find the channel number for.
-   @param coeffs The polynomial equation coefficients.
-   @param nchannel Then number of gamma channels the returned answer will have.
+   @param coeffs The FullRangeFraction equation coefficients.
+   @param nchannel Then number of gamma channels of the spectrum, must be at least 2.
    @param deviation_pairs The deviation pairs defined for the calibration; it is
           assumed they are sorted by energy already.
    @param accuracy The accuracy, in keV, the returned answer will be of the
           true answer; only used if deviation pairs are specified (otherwise
           answer is always exact)
    
-   Will throw exception if there are not a t least two coefficients, or if a
+   Will throw exception if there are not at least two coefficients, or if a
    solution is failed to be found (e.g., invalid coefficients or deviation
    pairs).
    
@@ -483,15 +509,45 @@ namespace SpecUtils
    binary search is performed to find the bin that comes within the specified
    accuracy.
    
-   ToDo: Use #correction_due_to_dev_pairs to make it so algabraic approach can
-         always be used.
-*/
+   \TODO: Use #correction_due_to_dev_pairs to make it so algabraic approach can
+          always be used.
+  */
   float find_fullrangefraction_channel( const double energy,
                                    const std::vector<float> &coeffs,
                                    const size_t nchannel,
                                    const std::vector<std::pair<float,float>> &deviation_pairs,
                                    const double accuracy = 0.001 );
   
+  /** Gives the channel (including fractional portion) corresponding to the
+     specified energy.
+     
+     @param energy The energy to find the channel number for.
+     @param coeffs The Polynomial equation coefficients.
+     @param nchannel Then number of gamma channels of the spectrum; used to help determine
+                     correct answer when multiple solutions exist.
+     @param deviation_pairs The deviation pairs defined for the calibration; it is
+            assumed they are sorted by energy already.
+     @param accuracy The accuracy, in keV, the returned answer will be of the
+            true answer; only used if coefficents are grater than third order (otherwise answer is
+            always exact). Must be greater than zero.
+     
+     Will throw exception if there are not at least two coefficients, or if a
+     solution is failed to be found (e.g., invalid coefficients or deviation
+     pairs).
+     
+     If number of coefficients is three or less, than an algabraic approach is used, otherwise a
+     binary search is performed to find the bin that comes within the specified accuracy.
+   
+     Note: that #correction_due_to_dev_pairs is used to correct for deviation pairs when using the,
+     algebraic appoach, and currently (20200820) may be correct to only 0.01 keV.
+  */
+  float find_polynomial_channel( const double energy,
+                                 const std::vector<float> &coeffs,
+                                 const size_t nchannel,
+                                 const std::vector<std::pair<float,float>> &deviation_pairs,
+                                 const double accuracy = 0.001 );
+
+
   
   /** Converts channel counts defined by one set of channel lower energies, to
    instead be defined by a different set of lower energies.

commit 21903cdaa67bfbd08eb61b0fc632529b7a99b2ce

Fix bug for writing PCF files, and update Python bindings. Commit 920c045e3b818138f58a040f8cd8d653ef3d5f27 introduced bug that caused writing to PCF files to fail - this has been fixed, but not extensively tested. Updated Python bindings for changes to C++.  Again, not extensively tested.

bindings/python/SpecFile_py.cpp
@@ -394,20 +394,22 @@ namespace
   
   std::shared_ptr<SpecUtils::Measurement> sum_measurements_wrapper( SpecUtils::SpecFile *info,
                                                           boost::python::list py_samplenums,
-                                                          boost::python::list py_detnums )
+                                                          boost::python::list py_detnames )
   {
     set<int> samplenums;
-    vector<int> detnums;
+    set<string> detnames;
     
     boost::python::ssize_t n = boost::python::len( py_samplenums );
     for( boost::python::ssize_t i = 0; i < n; ++i )
       samplenums.insert( boost::python::extract<int>( py_samplenums[i] ) );
     
-    n = boost::python::len( py_detnums );
+    n = boost::python::len( py_detnames );
     for( boost::python::ssize_t i = 0; i < n; ++i )
-      detnums.push_back( boost::python::extract<int>( py_detnums[i] ) );
+      detnames.insert( boost::python::extract<std::string>( py_detnames[i] ) );
     
-    return info->sum_measurements(samplenums, detnums );
+    const vector<string> detname_vec( begin(detnames), end(detnames) );
+
+    return info->sum_measurements( samplenums, detname_vec, nullptr );
   }//sum_measurements_wrapper(...)
   
   
@@ -780,9 +782,9 @@ enum_<SpecUtils::DetectorType>( "DetectorType" )
   .def( "reset", &SpecUtils::SpecFile::reset,
         "Resets the SpecUtils::SpecFile object to its initial (empty) state." )
   .def( "sumMeasurements", &sum_measurements_wrapper,
-        args("SampleNumbers", "DetectorNumbers"),
+        args("SampleNumbers", "DetectorNames"),
         "Sums the measurements of the specified sample and detector numbers.\n"
-        "SampleNumbers and DetectorNumbers are both lists of integers.\n"
+        "SampleNumbers is a list of integers and DetectorNames is a list of strings.\n"
         "If the measurements contain different energy binnings, one will be chosen\n"
         "and the other measurements rebinned before summing so that energies stay\n"
         "consistent (e.g. not just a bin-by-bin summing).\n"

src/SpecFile_pcf.cpp
@@ -137,6 +137,10 @@ namespace
 namespace SpecUtils
 {
   
+/** Gives the maximum number of channels any spectrum in the file will need to write to PCF file (rounded up to the nearest multiple of
+ 64 channels), as well as a sets a pointer to the lower channel energies to write to the first record, but only if lower channel energy
+ calibration should be used (if FRF should be used, then pointer will be reset to nulltr).
+ */
 void SpecFile::pcf_file_channel_info( size_t &nchannel,
                                        std::shared_ptr<const std::vector<float>> &lower_channel_energies ) const
 {
@@ -151,10 +155,9 @@ void SpecFile::pcf_file_channel_info( size_t &nchannel,
   for( const auto &meas : measurements_ )
   {
     const size_t nmeaschann = meas->num_gamma_channels();
+    
     const auto &cal = meas->energy_calibration_;
-    if( !cal
-        || (cal->type()==EnergyCalType::InvalidEquationType)
-        || (cal->num_channels() <= 7) )
+    if( !cal || (cal->type() == EnergyCalType::InvalidEquationType) || (nmeaschann <= 7) )
       continue;
     
     const size_t ncalchannel = cal->num_channels();
@@ -165,25 +168,22 @@ void SpecFile::pcf_file_channel_info( size_t &nchannel,
       
     nchannel = std::max( nchannel, nmeaschann );
     
-    if( (ncalchannel < nmeaschann)
-       || (meas->energy_calibration_model() != SpecUtils::EnergyCalType::LowerChannelEdge) )
+    if( meas->energy_calibration_model() != SpecUtils::EnergyCalType::LowerChannelEdge )
     {
       use_lower_channel = false;
-      nchannel = 0;
-      return;
+      continue;
     }
     
     //If we have already found a lower_e_bin, check if this current
-    //  one is either the same  one in memory, or if not, if its reasonably
+    //  one is either the same one in memory, or if not, if its reasonably
     //  close in numeric value.
-    if( use_lower_channel && lower_e_bin && (lower_e_bin!=these_energies) )
+    if( use_lower_channel && lower_e_bin && (lower_e_bin != these_energies) )
     {
       if( lower_e_bin->size() != these_energies->size() )
       {
         use_lower_channel = false;
         lower_e_bin.reset();
-        nchannel = 0;
-        return;
+        continue;
       }
       
       for( size_t channel = 0; use_lower_channel && channel < lower_e_bin->size(); ++channel )
@@ -197,24 +197,22 @@ void SpecFile::pcf_file_channel_info( size_t &nchannel,
         {
           use_lower_channel = false;
           lower_e_bin.reset();
-          nchannel = 0;
-          return;
+          continue;
         }
       }//for( size_t channel = 0; use_lower_channel && channel < lower_e_bin->size(); ++channel )
-    }else if( use_lower_channel && (lower_e_bin!=these_energies) )
+    }else if( use_lower_channel && (lower_e_bin != these_energies) )
     {
       lower_e_bin = these_energies;
     }//if( we already have found lower_e_bin ) / else if( we might us the lower channel energies )
   }//for( const auto &meas : measurements_ )
   
-  if( !use_lower_channel || nchannel <= 7 || !lower_e_bin || lower_e_bin->size() <= 7 )
+  if( nchannel <= 7 )
   {
     nchannel = 0;
     return;
   }
   
-  
-  if( use_lower_channel && nchannel > 7 && lower_e_bin && lower_e_bin->size() > 7 )
+  if( use_lower_channel && lower_e_bin && (lower_e_bin->size() > 7) )
   {
     nchannel += 1;  //GADRAS needs N+1 channels for the lower energy channels record
     if( lower_e_bin->size() == nchannel )
@@ -222,6 +220,7 @@ void SpecFile::pcf_file_channel_info( size_t &nchannel,
       lower_channel_energies = lower_e_bin;
     }else
     {
+      // After the great energy calibration re-factoring, I dont think we will ever get here...
       auto binning = make_shared<vector<float>>(nchannel);
       if( lower_e_bin->size() >= nchannel )
       {
@@ -620,7 +619,7 @@ bool SpecFile::write_pcf( std::ostream &outputstrm ) const
     
     
     std::basic_string<char> fileid;
-    int16_t NRPS = 1 + static_cast<int16_t>( 4.0*nchannel_file/256.0 );
+    const int16_t NRPS = 1 + static_cast<int16_t>( 4.0*nchannel_file/256.0 );
     //cout << "NRPS=" << int(NRPS) << endl;
     
     fileid.resize( 2, '\0' );

bindings/python/test_python.py
@@ -87,7 +87,7 @@ print "DetectorNumbers:", info.detectorNumbers()
 print "SampleNumbers:", info.sampleNumbers()
 
 
-summedmeas = info.sumMeasurements( [1,2], info.detectorNumbers() )
+summedmeas = info.sumMeasurements( [1,2], info.detectorNames() )
 print "Summed measurement has liveTime=", summedmeas.liveTime()
 
 